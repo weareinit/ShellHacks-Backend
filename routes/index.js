8770b9226f7b5c834d85d1728abe77e802653403
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-01 00:39:30 -0500

    Added JWT examples for auth and validation
    - Modified index.js to show the token signing flow
    - Modified hacker.js to show the calidation flow
    - Modified package.json to upgrade sqlite to v4 to support node 10
      - Lockfile was updated due to this too

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
+var jwt = require('jsonwebtoken');
 
 router.get('/', function(req, res) {
   models.User.findAll({
@@ -10,6 +11,27 @@ router.get('/', function(req, res) {
       title: 'Sequelize: Express Example',
       users: users
     });
+  });
+});
+
+router.post('/login', function(req, res) {
+  //If user's password checks out, we give them a JWT
+  //Logic to check if password is correct first go here
+
+  var token = jwt.sign(
+    //JSON/Object payload of Claims
+    {
+      name: "User",
+      //Token expires in an hour
+      exp: Math.floor(Date.now() / 1000) + (60 * 60),
+      admin: false
+    },
+    'SecretWeShouldBeStoringInAnEnvironmentVariableOrConfigFile'
+  );
+
+  //Send the token back in an object for the client to store
+  res.send({
+    token: token
   });
 });
 
