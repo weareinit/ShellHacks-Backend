349e880f08027b03f284b4beceda5c1761702536
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-12 20:34:23 -0500

    Added dotenv to config.js and set modules to use env variables for DB name

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.js')[env];
 var db        = {};
 
-var sequelize = new Sequelize('Shell', process.env.DB_USER, process.env.DB_PASS, {
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   dialect: 'mysql',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
