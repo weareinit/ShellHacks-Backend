6123242b1048cd02e4773377fa8afa4879dc4ad8
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-13 01:25:58 -0500

    Modify sponsors.js model

--- models/sponsors.js
+++ models/sponsors.js
@@ -1,6 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var sponsor = sequelize.define('sponsors', {}, {
+  var sponsor = sequelize.define('sponsors', {
+    name: DataTypes.STRING,
+    tier: DataType.STRING,
+    logo: DataType.BLOB('medium')
+  }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
