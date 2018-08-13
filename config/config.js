db7d2dbf9f68eacf07bfd925c5be26b972f7a9ce
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-12 23:47:23 -0500

    Added dev settings for sequelize

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "ShellHacks",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DEV_NAME,
     host: "127.0.0.1",
     dialect: 'mysql'
   },
